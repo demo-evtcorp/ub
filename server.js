550d8c2ffabb19abb2be682673fb03c891d2a6f4
khua <khua@redhat.com>
2017-05-17 23:13:13 -0700

    test

---
+++ server.js
@@ -0,0 +1,21 @@
+//  OpenShift sample Node application
+var express = require('express'),
+    fs      = require('fs'),
+    app     = express(),
+    eps     = require('ejs'),
+    morgan  = require('morgan');
+    
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+//app.get('/', function (req, res) {
+//    res.render('index.html', { pageCountMessage : null});
+//});
+
+app.get('*', view.index);
+
+app.listen(port, ip);
+//console.log('Server running on http://%s:%s', ip, port);
+console.log('Server running...');
+
+module.exports = app ;
